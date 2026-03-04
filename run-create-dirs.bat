f9de9fc259a21bd0dc437f85cd5e1ae0c0ae2f8d
yjcmsft <yjcmsft@microsoft.com>
2026-03-03 21:49:26 -0800

    cleanup

--- run-create-dirs.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\yingjungchen\Downloads\Secure-Enterprise-Browser-Agentic-System"
-node create-dirs.js
-pause
--- run-setup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\yingjungchen\Downloads\Secure-Enterprise-Browser-Agentic-System"
-call node setup-structure.js
-pause
